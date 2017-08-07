8c6bb2e3ed7780db2ea77637298df21e85456021
TomVanRoy <tom.van.roy@outlook.com>
2017-08-07 15:44:42 +0200

    Einde dag 2017/08/07

---
+++ MVC_Voorbeeld2/Controllers/WerknemerController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Voorbeeld2.Controllers
+{
+    public class WerknemerController : Controller
+    {
+        // GET: Werknemer
+        public ActionResult Index(int? id)
+        {
+            return View();
+            //return File(@"C:\kruisdistel.jpg", "image/jpg");
+        }
+
+        public ActionResult ShowAll(int? pageNr)
+        {
+            return View();
+        }
+
+        public ActionResult VerdubbelDeWeddes()
+        {
+            //update in de database
+            return Redirect("~/Werknemer/WeddesAangepast");
+        }
+
+        public ActionResult WeddesAangepast()
+        {
+            return View();
+        }
+
+        [NonAction]
+        public void GeenAction()
+        {
+        }
+    }
+}
