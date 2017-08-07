85104f711d374d697a86371920a5b9b37fd3d0d2
TomVanRoy <tom.van.roy@outlook.com>
2017-08-07 12:09:25 +0200

    Add project files.

---
+++ MVC_Voorbeeld1/Controllers/FiliaalController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Voorbeeld1.Controllers
+{
+    public class FiliaalController : Controller
+    {
+        // GET: Filiaal
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult List()
+        {
+            return View();
+        }
+
+        public ActionResult Read(int? id)
+        {
+            //lees in de database het filiaal waarvan de id gelijk is aan de waarde van de parameter id
+            return View();
+        }
+    }
+}
